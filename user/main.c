d2f46a18cae248cf2a418f7127b5e4da010c6311
Daniel Mu <ds1231h@163.com>
2016-07-30 05:15:17 +0800

    已增加中断

--- user/main.c
+++ user/main.c
@@ -5,7 +5,7 @@
 #include"ioRead.h"
 
 u8 table1[]="15Hz";
-u8 table2[]="5%";
+u8 table2[]="52.3%";
 u8 temp;
 // u8 rdIn[8];
 char resu[10];
@@ -48,7 +48,7 @@ void readIo(void)
 		p_resu--;
 	}
 	p_resu++;        //对字符串添加'\0'
-	for(*p2_resu=resu[0];p2_resu !=&resu[11];)   //将字符串右对齐变成左对齐
+	for(*p2_resu=resu[0];p2_resu !=&resu[10];)   //将字符串右对齐变成左对齐
   {
     *p2_resu = *p_resu;
     p2_resu++;
@@ -57,13 +57,99 @@ void readIo(void)
 	*p2_resu = '\0';
 }
 
+//配置系统时钟************************************************************
+void RCC_Configuration(void)
+{
+	/* TIM2 clock enable */ 
+ RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); 
+  
+ /* GPIOB clock enable */ 
+ // RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); 
+	
+}
+
+//外部中断函数*************************************************************************************
+void EXTI_Configuration(void)
+{
+	EXTI_InitTypeDef EXTI_InitStructure;	              //初始化外部中断寄存器
+
+	EXTI_ClearITPendingBit(EXTI_Line1);					  //清除线1 IO口中断清除挂起位（清除中断标志位）
+	EXTI_InitStructure.EXTI_Mode=EXTI_Mode_Interrupt;	  //设置外部中断触发（另一种是事件触发）
+	EXTI_InitStructure.EXTI_Trigger=EXTI_Trigger_Falling; //设置中断触发方式：下降沿触发方式
+	EXTI_InitStructure.EXTI_Line=EXTI_Line1;			  //选择中断线路为1（即选择那个IO作为中断输入）
+	EXTI_InitStructure.EXTI_LineCmd=ENABLE;				  //使能外部中断
+	EXTI_Init(&EXTI_InitStructure);                       //初始化
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB,GPIO_PinSource1); //将GPIOB1挂到中断上
+	
+	
+	
+	EXTI_ClearITPendingBit(EXTI_Line2);					  //清除线2 IO口中断清除挂起位（清除中断标志位）
+	EXTI_InitStructure.EXTI_Mode=EXTI_Mode_Interrupt;	  
+	EXTI_InitStructure.EXTI_Trigger=EXTI_Trigger_Rising; //设置中断触发方式：上升沿触发方式
+	EXTI_InitStructure.EXTI_Line=EXTI_Line2;			  //选择中断线路为2
+	EXTI_InitStructure.EXTI_LineCmd=ENABLE;				  //使能外部中断
+	EXTI_Init(&EXTI_InitStructure);                       //初始化
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB,GPIO_PinSource2); //将GPIOB2挂到中断上
+
+}
+
+//中断分组函数***************************************************************
+void NIVC_Configuration(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure;                  //初始化中断分组函数
+
+//NVIC_PriorityGroupConfig:设置优先级分组（下面一句）
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);        //设置优先级：抢先优先级0个，子优先级4位（16个）
+	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;		 // 使能设置的外部中断通道请求
+	
+	NVIC_InitStructure.NVIC_IRQChannel=EXTI1_IRQn;     //将中断挂到PB1脚外部中断线1上
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=1;	 //设置子优先级1
+	NVIC_Init(&NVIC_InitStructure);                   	 //初始化
+	
+	
+	
+	NVIC_InitStructure.NVIC_IRQChannel=EXTI2_IRQn;     //将中断挂到PB2脚外部中断线2上
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=2;	 //设置子优先级2，要低于前面的子优先级1
+	NVIC_Init(&NVIC_InitStructure);                   	 //初始化
+	
+	
+}
+
+
+//中断服务函数********************************************************
+void EXTI1_IRQHandler(void)
+{
+	if(EXTI_GetITStatus(EXTI_Line1)!=RESET)//有中断申请
+	{
+		//添加中断处理函数
+		GPIO_ResetBits(GPIOB,GPIO_Pin_8);     //点亮led
+		EXTI_ClearFlag(EXTI_Line1);			 //清除标志中断位
+		EXTI_ClearITPendingBit(EXTI_Line1);	 //清除外部中断线1的挂起位
+	}
+}
+
+void EXTI2_IRQHandler(void)
+{
+	if(EXTI_GetITStatus(EXTI_Line2)!=RESET)
+	{
+		//添加中断处理函数
+		GPIO_ResetBits(GPIOB,GPIO_Pin_9); 
+		EXTI_ClearFlag(EXTI_Line2);			 
+		EXTI_ClearITPendingBit(EXTI_Line2);	 //清除外部中断线2的挂起位
+	}
+}
+
 int main(void)
 {
 	u8 a;
 	delay_init();
-	uart_init(9600);
 	GPIO_1602_Init();
 	LCD_1602_Init();
+	
+	RCC_Configuration();				   //调用系统时钟函数
+	NIVC_Configuration();
+	EXTI_Configuration();
+	
 	Write_Com(0x80+17);
 	delay_ms(200);
 	
